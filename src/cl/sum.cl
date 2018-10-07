238cf10e8e35c0306c03ca10501455cd74b7a096
Dmitry Boykiy <dmitry.boykiy@soteranalytics.com>
2018-10-08 00:44:13 +0300

    Add sum

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,31 @@
-// TODO
+#ifdef __CLION_IDE__
+
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#line 6
+
+#define WORK_GROUP_SIZE 256
+
+__kernel void sum(__global const int *xs, int n, __global int *res) {
+
+    int localId = get_local_id(0);
+    int globalId = get_global_id(0);
+
+    __local int local_xs[WORK_GROUP_SIZE];
+    local_xs[localId] = xs[globalId];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int nvalues = WORK_GROUP_SIZE; nvalues > 1; nvalues /= 2) {
+        if (2 * localId < nvalues) {
+            local_xs[localId] += local_xs[localId + nvalues / 2];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (localId == 0) {
+        atomic_add(res, local_xs[0]);
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,11 +1,13 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#include "cl/sum_cl.h"
 
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
+void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
     if (a != b) {
         std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
@@ -15,12 +17,11 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int benchmarkingIters = 10;
 
     unsigned int reference_sum = 0;
-    unsigned int n = 100*1000*1000;
+    unsigned int n = 100 * 1000 * 1000;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(42);
     for (int i = 0; i < n; ++i) {
@@ -39,14 +40,14 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU:     " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             unsigned int sum = 0;
-            #pragma omp parallel for reduction(+:sum)
+#pragma omp parallel for reduction(+:sum)
             for (int i = 0; i < n; ++i) {
                 sum += as[i];
             }
@@ -54,11 +55,43 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU OMP: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
+        // Этот контекст после активации будет прозрачно использоваться при всех вызовах в libgpu библиотеке
+        // это достигается использованием thread-local переменных, т.е. на самом деле контекст будет активирован для текущего потока исполнения
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        gpu::gpu_mem_32u as_gpu;
+        as_gpu.resizeN(n);
+        as_gpu.writeN(as.data(), as.size());
+
+        gpu::gpu_mem_32u res_gpu;
+        res_gpu.resizeN(1);
+
+        ocl::Kernel sum_kern(sum_kernel, sum_kernel_length, "sum");
+        sum_kern.compile();
+
+        unsigned int workGroupSize = 256;
+        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            unsigned int sum = 0;
+            res_gpu.writeN(&sum, 1);
+            sum_kern.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                          as_gpu, n, res_gpu);
+            res_gpu.readN(&sum, 1);
+            EXPECT_THE_SAME(reference_sum, sum, "CPU OpenMP result should be consistent!");
+            t.nextLap();
+        }
+
+        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 }
